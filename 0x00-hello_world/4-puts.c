65f191544581b263fe00040a50042956f45f5d2a
‘ChukwumaPrecious’ <chukwumapee@gmail.com>
2022-09-08 19:27:52 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
+ *   main - Entry point
  *
- *  * main - main block
+ *   * Return: Always 0 (Success)
  *
- *   * Return: 0
- *
- *    */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
