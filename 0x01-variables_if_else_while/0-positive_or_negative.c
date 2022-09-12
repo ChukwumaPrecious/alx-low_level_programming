1cf1d44698502120deba281abf4470babe1f90f3
‘ChukwumaPrecious’ <chukwumapee@gmail.com>
2022-09-12 15:26:12 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,34 +3,24 @@
 #include <stdio.h>
 
 /**
-   * main - checks if positive, zero or negative
-   *
-   *Return: zero
-   */
+* main - checks if positive, zero or negative
+*
+*Return: zero
+*/
 int main(void)
-
 {
-
 	int n;
+
         srand(time(0));
         n = rand() - RAND_MAX / 2;
+
         if (n > 0)
-        {
-
 		printf("%d is positive\n", n);
-
-	} else if (n == 0)
-
-	{															
+	else if (n == 0)
 		printf("%d is zero\n", n);
-																	} else
-																	{
-																		printf("%d is negative\n", n);
+	else
+		printf("%d is negative\n", n);
 
-																	}
-
-																	return (0);
+	return (0);
 
 }
-
-
